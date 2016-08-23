9af163ea24baa7b6103ad2973d2e9d89e86e11b7
jbenjaminy <young.benjamin@gmail.com>
2016-08-23 14:05:21 -0500

    finishes commented code

---
+++ interview-questions.js
@@ -0,0 +1,3 @@
+/* Write an algorithm to check whether an arbitrary tree is a binary search tree */
+/* Write an algorithm to find the height of a binary search tree */
+/* Write an algorithm to find the third largest value in a binary search tree */
